cfa62e4226a0d744f444423b7fe31386a8c017ce
MargaritaZhuk <45733303+MargaritaZhuk@users.noreply.github.com>
2025-01-26 19:37:47 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,17 @@
 <script>
-    document.getElementById('toggleButton').addEventListener('click', function() {
+     document.getElementById('toggleButton').addEventListener('click', function() {
         const hiddenText = document.getElementById('hiddenText');
         hiddenText.style.display = 'block';
         this.disabled = true; // Отключаем кнопку после нажатия
-        this.textContent = 'Текст отображен';
+        this.textContent = 'Мой опыт';
     });
+    
+    document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+});
 </script>
